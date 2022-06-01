4c1910ddd94606abe4074d210f50a7cc2abab71c
vanesasole <gvanesad98@gmail.com>
2022-06-01 18:31:38 -0300

    html de home y css. Modificacion de app.js

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express=require ('express');
-const path = require ("path");
+const path = require ('path');
 let app =express();
+
+const publicPath = path.resolve(__dirname, './public');
+app.use(express.static(publicPath) );
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo en el puerto 3000');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/home.html'));
+})
