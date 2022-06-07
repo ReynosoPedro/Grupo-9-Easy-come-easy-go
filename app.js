9e21e1647e123d94bf7e60078d9d28da9a968354
Pedro Reynoso <pedro.reynoso.2022@gmail.com>
2022-06-07 19:19:57 -0300

    heroku test modificado app.js

--- app.js
+++ app.js
@@ -5,9 +5,9 @@ let app =express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath) );
 
-app.listen(3000, () => {
-    console.log('Servidor corriendo en el puerto 3000');
-});
+app.listen(process.env.PORT || 3000 , function (){
+    console.log("Servidor corriendo en el puerto 3000");
+})
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
