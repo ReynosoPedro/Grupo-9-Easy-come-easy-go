53fa5a244e183b1a6459195bcb54d5f4823b78bf
Your Name <you@example.com>
2022-06-07 20:31:29 -0300

    ultimos cambios

--- app.js
+++ app.js
@@ -5,8 +5,12 @@ let app =express();
 const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath) );
 
-app.listen(process.env.PORT || 3000 , function (){
+app.listen(process.env.PORT || 3050 , function (){
     console.log("Servidor corriendo en el puerto 3000");
+})
+
+app.get('/productos', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/productos.html'));
 })
 
 app.get('/', (req, res) => {
@@ -15,5 +19,8 @@ app.get('/', (req, res) => {
 
 app.get('/register', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/register.html'));
+})
+app.get('/shop-car', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './views/shop-car.html'));
 })
 
