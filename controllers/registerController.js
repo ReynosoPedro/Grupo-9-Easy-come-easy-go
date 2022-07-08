bdd4e68d208644bc22b473140ca6779bbb2a3547
andyrov <100967425+andyrov@users.noreply.github.com>
2022-07-07 21:24:46 -0300

    json register

--- controllers/registerController.js
+++ controllers/registerController.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const fs = require('fs');
+const usuariosJSON =  JSON.parse(fs.readFileSync(path.resolve(__dirname,'../database/users.json')));
 const register = {
     register: (req, res) => {
         res.render('register');
@@ -6,16 +9,19 @@ const register = {
        let user = {
         nombre: req.body.nombreCompleto,
         usuario: req.body.usuario,
-        contraseña: req.body.contraseña,
-        confirmarContraseña: req.body.confirmarContraseña,
+        password: req.body.contraseña,
+        confirmPassword: req.body.confirmarContraseña,
         fechaDeNacimiento: req.body.fechaDeNacimiento,
         email: req.body.email,
         celular: req.body.celular,
         terminos: req.body.terminos,
         newsletter: req.body.newsletter
        }
+       const nuevosUsuarios = usuariosJSON.push(user)
+       JSON.stringify(nuevosUsuarios)
+        fs.writeFileSync(path.resolve(__dirname,'..','database','users.json'), nuevosUsuarios);
         res.redirect('/login')
-    }
+    }   
 };
 
 module.exports = register
