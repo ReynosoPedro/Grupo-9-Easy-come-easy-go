a07e68e5e9fc508954005ec23596c1ee5c95a18f
vanesasole <gvanesad98@gmail.com>
2022-08-24 22:17:07 -0300

    letra

--- database/models/Km_intervalo.js
+++ database/models/Km_intervalo.js
@@ -6,7 +6,7 @@ module.exports = (sequelize, dataTypes)=>{
             primaryKey: true,
             autoIncrement: true
         },
-        interval:{
+        intervals:{
             type: dataTypes.DECIMAL
         },
         
--- database/models/Producto.js
+++ database/models/Producto.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, dataTypes)=>{
         transmission:{
             type:dataTypes.STRING
         },
-        condition:{
+        conditions:{
             type:dataTypes.STRING
         },
         stock:{
