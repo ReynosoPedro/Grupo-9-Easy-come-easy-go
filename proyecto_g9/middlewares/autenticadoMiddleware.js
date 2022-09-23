3259c746c8d3ac4383894d91f2661de477f7363e
Pedro Reynoso <pedro.reynoso.2022@gmail.com>
2022-09-23 13:14:40 -0300

    reestructurando

--- proyecto_g9/middlewares/autenticadoMiddleware.js
+++
@@ -1,8 +0,0 @@
-function autenticadoMiddleware(req, res, next) {
-	if (req.session.userLogged==undefined || req.session.userLogged.state!=9) {
-		return res.redirect('/login');
-	}
-	next();
-}
-
-module.exports = autenticadoMiddleware;
