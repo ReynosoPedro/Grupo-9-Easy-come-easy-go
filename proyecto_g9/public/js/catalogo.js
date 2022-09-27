bc208e6beeed028ccb7f047d3db3838b4243de22
Pedro Reynoso <pedro.reynoso.2022@gmail.com>
2022-09-27 20:28:10 -0300

    aldito header

--- proyecto_g9/public/js/catalogo.js
+++ proyecto_g9/public/js/catalogo.js
@@ -1,4 +1,16 @@
 window.onload = function(){
+    
+    let menuHam = document.querySelector('.burger-menu')
+    
+    menuHam.addEventListener('click', function(){
+
+        document.getElementById('menu-hamb').classList.toggle('burger-mostrar')
+    })
+
+    let burgerToggle = document.querySelector('#menu-hamb')
+    burgerToggle.addEventListener('click', function(){
+        document.getElementById('menu-hamb').classList.toggle('burger-mostrar')
+    })
 
     fetch("http://localhost:3050/api/products")
     .then(function(respuesta){
--- proyecto_g9/public/js/formularioVentas.js
+++ proyecto_g9/public/js/formularioVentas.js
@@ -65,6 +65,19 @@ function filterModel(){
 
 window.onload = function(){
 
+    let menuHam = document.querySelector('.burger-menu')
+    
+    menuHam.addEventListener('click', function(){
+
+        document.getElementById('menu-hamb').classList.toggle('burger-mostrar')
+    })
+
+    let burgerToggle = document.querySelector('#menu-hamb')
+    burgerToggle.addEventListener('click', function(){
+        document.getElementById('menu-hamb').classList.toggle('burger-mostrar')
+    })
+
+
     
     
 
