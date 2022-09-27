3cbf770b2bbe9cdf9d17a0cb3ee81cf4a5077a65
Pedro Reynoso <pedro.reynoso.2022@gmail.com>
2022-09-27 20:57:32 -0300

    mas correcciones en el header , todavia falta

--- proyecto_g9/public/js/productosedit.js
+++ proyecto_g9/public/js/productosedit.js
@@ -31,6 +31,18 @@ function filterModel(){
 
 
 window.onload = function(){
+    
+    let menuHam = document.querySelector('.burger-menu')
+    
+    menuHam.addEventListener('click', function(){
+
+        document.getElementById('menu-hamb').classList.toggle('burger-mostrar')
+    })
+
+    let burgerToggle = document.querySelector('#menu-hamb')
+    burgerToggle.addEventListener('click', function(){
+        document.getElementById('menu-hamb').classList.toggle('burger-mostrar')
+    })
 
     let form = document.querySelector('form')
     form.marca.focus()
